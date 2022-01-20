66b31b92a72eed09fde96bd86bfda17f4e0541d4
Ramadito Ferdian Assa <ramaditoferdian@gmail.com>
2022-01-20 20:05:33 +0700

    update admin

---
+++ app/Http/Controllers/ProductCategoryController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Models\ProductCategory;
+use Yajra\DataTables\Facades\DataTables;
+
+use App\Http\Requests\ProductCategoryRequest;
+
+class ProductCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+        if (request()->ajax()) {
+            $query = ProductCategory::query();
+
+            return DataTables::of($query)
+                ->addColumn('action', function($item){
+                    return '<a class="inline-block bg-gray-700 border border-gray-700 hover:bg-gray-800 text-white py-1 px-3 m-1 transition duration-500 ease select-none rounded-md focus:outline-none focus:shadow-outline" href="'.route('dashboard.category.edit', $item->id).'">Edit</a> ';
+                })
+
+                ->rawColumns(['action'])
+                ->make();
+
+        }
+
+        return view('pages.dashboard.category.index');
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductCategoryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProductCategory  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductCategory $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProductCategory  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductCategory $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProductCategory  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProductCategoryRequest $request, ProductCategory $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProductCategory  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductCategory $category)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductCategoryController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function(){
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+    Route::name('dashboard.')->prefix('dashboard')->group(function(){
+        Route::get('/', [DashboardController::class, 'index'])-> name('index');
+
+        Route::middleware(['admin'])->group(function(){
+            Route::resource('category', ProductCategoryController::class);
+        });
+
+    });
+
+});
